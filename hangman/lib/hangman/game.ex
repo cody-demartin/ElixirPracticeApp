3086000d4d27520ffe926c1ac05db5a021bf7cc5
cdemarti <codythomasdemartin@gmail.com>
2020-12-24 18:12:25 -0500

    adds already guessed check and tests

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -2,7 +2,8 @@ defmodule Hangman.Game do
   defstruct(
     turns_left: 7,
     game_state: :initializing,
-    letters: []
+    letters: [],
+    used: MapSet.new()
   )
 
   def new_game() do
@@ -11,13 +12,24 @@ defmodule Hangman.Game do
     }
   end
 
-  def make_move(game = %{ game_state: state}, _guess) when state in [:won, :lost] do
-    { game, tally(game) }
+  def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
+    {game, tally(game)}
+  end
+
+  def make_move(game, guess) do
+    game = accept_move(game, guess, MapSet.member?(game.used, guess))
+    {game, tally(game)}
+  end
+
+  def accept_move(game, _guess, _already_guessed = true) do
+    Map.put(game, :game_state, :already_used)
+  end
+
+  def accept_move(game, guess, _alredy_guessed) do
+    Map.put(game, :used, MapSet.put(game.used, guess))
   end
 
   def tally(_game) do
     1234
   end
-
-
 end

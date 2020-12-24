41e82028a58754f121f1632f39a0ee9250631010
cdemarti <codythomasdemartin@gmail.com>
2020-12-24 14:45:27 -0500

    adds recursive list notes

---
+++ lib/lists.ex
@@ -0,0 +1,29 @@
+defmodule Lists do
+    
+    # []
+    def len([]), do: 0
+    # any other list
+    def len([_h | t ]), do: 1 + len(t)
+
+    # []
+    def sum([]), do: 0
+
+    # any other list
+    def sum([ h | t ]), do: h + sum(t)
+
+    # []
+    def double([]), do: []
+    # any other list
+    def double([ h | t ]), do:  [2 * h | double(t)]
+
+    # []
+    def square([]), do: []
+    # any other list
+    def square([ h | t ]), do: [h * h | square(t)]
+
+    def map([], _func), do: []
+    def map([ h | t ], func) do
+        [ func.(h) | map(t, func) ]
+    end
+    
+end

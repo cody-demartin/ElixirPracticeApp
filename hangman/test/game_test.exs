096d1f722f77c380bee2e6e98451e8fa72410988
cdemarti <codythomasdemartin@gmail.com>
2020-12-27 23:24:55 -0500

    refactoring tests work

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -11,12 +11,12 @@
 #     assert length(game.letters) > 0
 #   end
 
-#   test "state isn't changed for :won or :lost game" do
-#     for state <- [:won, :lost] do
-#       game = Game.new_game() |> Map.put(:game_state, state)
-#       assert {^game, _} = Game.make_move(game, "x")
-#     end
-#   end
+  test "state isn't changed for :won or :lost game" do
+    for state <- [:won, :lost] do
+      game = Game.new_game() |> Map.put(:game_state, state)
+      assert {^game, _} = Game.make_move(game, "x")
+    end
+  end
 
 #   test "first occurence of letter is not already used" do
 #     game = Game.new_game()

8ea765b7a473b19ff72726117a270021fd76935d
cdemarti <codythomasdemartin@gmail.com>
2021-01-06 14:08:25 -0500

    implements logic to continue after good ro bad guess

--- text_client/lib/text_client/player.ex
+++ text_client/lib/text_client/player.ex
@@ -12,16 +12,29 @@ defmodule TextClient.Player do
     exit_with_message("Sorry you lost loser!")
   end
 
-  def play(%State{tally: %{ game_state: :good_guess} }) do
-    IO.puts("Good guess!")
+  def play(game = %State{tally: %{ game_state: :good_guess} }) do
+    continue_with_message(game, "Good guess!")
   end
 
-  def play(%State{tally: %{ game_state: :bad_guess} }) do
-    IO.puts("Sorry that isn't in the word.")
+  def play(game = %State{tally: %{ game_state: :bad_guess} }) do
+    continue_with_message(game, "Sorry bad guess!")
   end
 
   defp exit_with_message(msg) do
     IO.puts(msg)
     exit(:normal)
   end
+
+  def continue_with_message(game, message) do
+    IO.puts(message)
+    continue(game)
+  end
+
+  def continue(game) do
+
+
+  end
+
+
+
 end

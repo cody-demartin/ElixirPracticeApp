7e48fec335adca288b7ff78d1b14a2ea0a888275
cdemarti <codythomasdemartin@gmail.com>
2021-01-06 15:15:52 -0500

    Adds letters guessed line to CLI

--- text_client/lib/text_client/summary.ex
+++ text_client/lib/text_client/summary.ex
@@ -1,11 +1,13 @@
 defmodule TextClient.Summary do
-  def display(game = %{tally: tally}) do
+  def display(game = %{game_service: state, tally: tally}) do
     IO.puts([
       "\n",
       "Word so far: #{Enum.join(tally.letters, " ")}\n",
-      "Guesses left: #{tally.turns_left}\n"
+      "Guesses left: #{tally.turns_left}\n",
+      "Letters guessed: #{MapSet.to_list(state.used) |> Enum.map( fn x -> "#{x} " end)}\n"
     ])
 
     game
   end
+
 end

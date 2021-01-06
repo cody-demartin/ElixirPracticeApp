58aa9c7a24372d9e26356e3418f04cf79bef5123
cdemarti <codythomasdemartin@gmail.com>
2021-01-06 14:01:31 -0500

    adds case for lost game

--- text_client/lib/text_client/player.ex
+++ text_client/lib/text_client/player.ex
@@ -5,7 +5,15 @@ defmodule TextClient.Player do
 
   # won, lost, good guess, bad guess, already used, initializing - cases
   def play(%State{tally: %{ game_state: :won} }) do
-    IO.puts "You WON!"
+    exit_with_message("You won!")
+  end
+
+  def play(%State{tally: %{ game_state: :lost} }) do
+    exit_with_message("Sorry you lost loser!")
+  end
+
+  defp exit_with_message(msg) do
+    IO.puts(msg)
     exit(:normal)
   end
 end

19cf59151dc32c82fe168a03556b011518089168
cdemarti <codythomasdemartin@gmail.com>
2021-01-13 16:16:21 -0500

    refactors textclient api

--- text_client/lib/text_client/summary.ex
+++ text_client/lib/text_client/summary.ex
@@ -1,12 +1,10 @@
 defmodule TextClient.Summary do
-  def display(game = %{game_service: state, tally: tally}) do
+  def display(game = %{game_service: _state, tally: tally}) do
     IO.puts([
       "\n",
       "Word so far: #{Enum.join(tally.letters, " ")}\n",
       "Guesses left: #{tally.turns_left}\n",
-      "Letters guessed: #{MapSet.to_list(state.used) |> Enum.map( fn x -> "#{x} " end)}\n"
-    ])
-
+      ])
     game
   end
 
